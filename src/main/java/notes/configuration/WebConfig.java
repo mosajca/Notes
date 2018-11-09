7f3b903168679ce72046fdc4b1c8e1da191ae444
Marcin Osajca <marcin9608@gmail.com>
2018-11-09 18:50:14 +0100

    add NoteRepository and favicon

--- src/main/java/notes/configuration/WebConfig.java
+++ src/main/java/notes/configuration/WebConfig.java
@@ -5,16 +5,23 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @EnableWebMvc
 @ComponentScan("notes")
 @Configuration
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer {
 
     @Bean
     public ViewResolver resolver() {
         return new InternalResourceViewResolver("/WEB-INF/view/", ".jsp");
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/resources/");
     }
 
 }
